517ac5756e9743ca53cce5a8e8482fde40265dcd
Fabio Santos <fabiodanielmonteirosantos@gmail.com>
2019-11-05 18:49:26 +0000

    Create elastic-ip-manager lambda

---
+++ envs/prod/common/common.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region  = var.region
+}
+
+terraform {
+  backend "s3" {
+    region  = "eu-west-1"
+    bucket  = "fsantos-k8s-terraform-state"
+    key     = "envs/prod/common/terraform.tfstate"
+    encrypt = "true"
+  }
+}
+
+module "elastic-ip-manager" {
+  source = "../../../modules/project/elastic-ip-manager"
+  region = var.region
+}
