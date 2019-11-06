bca63c776dc8623885337aec26721bced41efb19
Fabio Santos <fabiodanielmonteirosantos@gmail.com>
2019-11-06 17:38:29 +0000

    Add dns support to vpc

--- envs/prod/vpc/vpc.tf
+++ envs/prod/vpc/vpc.tf
@@ -21,9 +21,11 @@ module "vpc" {
   private_subnets = var.private_subnets
   public_subnets  = var.public_subnets
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
   one_nat_gateway_per_az = false
+  enable_dns_hostnames   = true
+  enable_dns_support     = true
 
   tags = {
     Terraform   = "true"
